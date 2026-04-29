8a653a3877973da466f25db305622492e1c36601
Eduardo Kern <edukern@gmail.com>
2026-04-29 00:48:50 -0300

    feat: add Style tab controls (accent color, typography, background, border radius)

--- simple-podcast-player/widgets/podcast-player.php
+++ simple-podcast-player/widgets/podcast-player.php
@@ -54,6 +54,47 @@ class Podcast_Player extends Widget_Base {
         ] );
 
         $this->end_controls_section();
+
+        $this->start_controls_section( 'section_style', [
+            'label' => esc_html__( 'Style', 'simple-podcast-player' ),
+            'tab'   => Controls_Manager::TAB_STYLE,
+        ] );
+
+        $this->add_control( 'accent_color', [
+            'label'     => esc_html__( 'Accent Color', 'simple-podcast-player' ),
+            'type'      => Controls_Manager::COLOR,
+            'default'   => '#333333',
+            'selectors' => [
+                '{{WRAPPER}} .spp-player' => '--spp-accent: {{VALUE}};',
+            ],
+        ] );
+
+        $this->add_group_control( Group_Control_Typography::get_type(), [
+            'name'     => 'title_typography',
+            'selector' => '{{WRAPPER}} .spp-title',
+        ] );
+
+        $this->add_control( 'background_color', [
+            'label'     => esc_html__( 'Background', 'simple-podcast-player' ),
+            'type'      => Controls_Manager::COLOR,
+            'default'   => '#ffffff',
+            'selectors' => [
+                '{{WRAPPER}} .spp-player' => 'background-color: {{VALUE}};',
+            ],
+        ] );
+
+        $this->add_control( 'border_radius', [
+            'label'      => esc_html__( 'Border Radius', 'simple-podcast-player' ),
+            'type'       => Controls_Manager::SLIDER,
+            'size_units' => [ 'px' ],
+            'range'      => [ 'px' => [ 'min' => 0, 'max' => 50 ] ],
+            'default'    => [ 'size' => 6, 'unit' => 'px' ],
+            'selectors'  => [
+                '{{WRAPPER}} .spp-player' => 'border-radius: {{SIZE}}{{UNIT}};',
+            ],
+        ] );
+
+        $this->end_controls_section();
     }
 
     protected function render() {
